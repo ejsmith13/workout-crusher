7700f64e0118e7d046219fc952a2ea599430d80d
Code JSON <73917308+codragon2020@users.noreply.github.com>
2021-03-06 14:58:50 -0500

    Nonworking deployment

--- config/config.js
+++ config/config.js
@@ -1,25 +1,22 @@
 const dotenv = require("dotenv");
+const mysql = require("mysql");
+var connection;
 
 // Call dotenv configuration
 dotenv.config();
 
-module.exports = {
-  development: {
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
     username: "root",
     password: process.env.password,
     database: "workout_db",
-    host: "127.0.0.1",
+    host: "localhost",
     dialect: "mysql"
-  },
-  test: {
-    username: "root",
-    password: null,
-    database: "workout_db",
-    host: "127.0.0.1",
-    dialect: "mysql"
-  },
-  production: {
-    use_env_variable: "JAWSDB_URL", // eslint-disable-line
-    dialect: "mysql"
-  }
-};
+  });
+}
+
+// connection.connect();
+
+module.exports = connection;
