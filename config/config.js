9f424b9d0b5d6498b92125999a4ff372af7278ec
Code JSON <73917308+codragon2020@users.noreply.github.com>
2021-03-07 10:34:16 -0500

    Updated heroku config and WORKS!

--- config/config.js
+++ config/config.js
@@ -1,22 +1,35 @@
-const dotenv = require("dotenv");
-const mysql = require("mysql");
-var connection;
+// const dotenv = require("dotenv");
+// const mysql = require("mysql");
+// var connection;
 
-// Call dotenv configuration
-dotenv.config();
+// // Call dotenv configuration
+// dotenv.config();
 
-if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-  connection = mysql.createConnection({
-    username: "root",
-    password: process.env.password,
-    database: "workout_db",
-    host: "localhost",
-    dialect: "mysql"
-  });
-}
+// if (process.env.JAWSDB_URL) {
+//   connection = mysql.createConnection(process.env.JAWSDB_URL);
+// } else {
+//   connection = mysql.createConnection({
+//     development: {
+//       username: "root",
+//       password: process.env.password,
+//       database: "passport_demo",
+//       host: "127.0.0.1",
+//       dialect: "mysql"
+//     },
+//     test: {
+//       username: "root",
+//       password: null,
+//       database: "database_test",
+//       host: "127.0.0.1",
+//       dialect: "mysql"
+//     },
+//     production: {
+//       use_env_variable: "JAWSDB_URL", // eslint-disable-line
+//       dialect: "mysql"
+//     }
+//   });
+// }
 
 // connection.connect();
 
-module.exports = connection;
+// module.exports = connection;
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 // Requiring necessary npm packages
+require("dotenv").config();
 const express = require("express");
 const session = require("express-session");
 // Requiring passport as we've configured it
@@ -25,7 +26,7 @@ require("./routes/html-routes.js")(app);
 require("./routes/api-routes.js")(app);
 
 // Syncing our database and logging a message to the user upon success
-db.sequelize.sync().then(() => {
+db.sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => {
     console.log(
       "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
