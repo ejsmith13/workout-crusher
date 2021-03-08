148b8a3da2a18b463131166fa84290952cfb08f1
Code JSON <73917308+codragon2020@users.noreply.github.com>
2021-03-07 21:33:06 -0500

    Updating html-routes and public/js login and signup files to point to new Workout page

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -8,22 +8,31 @@ module.exports = function(app) {
   app.get("/", (req, res) => {
     // If the user already has an account send them to the members page
     if (req.user) {
-      res.redirect("/members");
+      res.redirect("/newWorkout");
     }
     res.sendFile(path.join(__dirname, "../public/signup.html"));
+  });
+
+  app.get("/newWorkout", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/newWorkout.html"));
+  });
+
+  app.get("/workout/:id", (req, res) => {
+    console.log("ID Route", req.params.id);
+    res.sendFile(path.join(__dirname, "../public/newWorkout.html"));
   });
 
   app.get("/login", (req, res) => {
     // If the user already has an account send them to the members page
     if (req.user) {
-      res.redirect("/members");
+      res.redirect("/newWorkout");
     }
     res.sendFile(path.join(__dirname, "../public/login.html"));
   });
 
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
-  app.get("/members", isAuthenticated, (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/members.html"));
+  app.get("/newWorkout", isAuthenticated, (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/newWorkout.html"));
   });
 };
