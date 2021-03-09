fadd36c54ddbe97fc9d23c94a0eea10fefbcab9e
Daniel-Edwards56 <73670462+Daniel-Edwards56@users.noreply.github.com>
2021-03-08 21:05:18 -0500

    fixed config file and env setup dleleted

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 // Requiring necessary npm packages
 require("dotenv").config();
+console.log(process.env);
+
 const express = require("express");
 const session = require("express-session");
 // Requiring passport as we've configured it
