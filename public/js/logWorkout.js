16debf2fb51fef940e4c879278bc5d59754b9dcd
Daniel-Edwards56 <73670462+Daniel-Edwards56@users.noreply.github.com>
2021-03-10 22:05:28 -0500

    got started on the log workout page and displaying info

---
+++ public/js/logWorkout.js
@@ -0,0 +1,69 @@
+// const workout_entry = `
+// <tr>
+// <th scope="row">12/24/2020</th>
+// <td>Cookie Eating</td>
+// <td>25 reps of lifting cookies to my mouth</td>
+// <td>Resistance</td>
+// </tr>
+// `;
+// const element = document.getElementById("entry");
+// element.innerHTML = workout_entry;
+
+// Get all the books
+
+fetch("/api/exercise", {
+  method: "GET",
+  headers: {
+    "Content-Type": "application/json"
+  }
+})
+  .then((response) => response.json())
+  .then((data) => {
+    // data = [
+    //   {
+    //     date: "12/2/2020",
+    //     workout: "pushups",
+    //     description: "12 pushups",
+    //     category: "resistance"
+    //   },
+    //   {
+    //     date: "12/2/2020",
+    //     workout: "pushups",
+    //     description: "12 pushups",
+    //     category: "resistance"
+    //   }
+    // ];
+    console.log("Success in getting all exercises:", data);
+    data.forEach(({ date, workout, description, category }, i) => {
+      console.log(date);
+      const entryContainer = document.getElementById("entry");
+      const sectionBreak = document.createElement("hr");
+
+      const entrySection = document.createElement("tr");
+      //   entrySection.classList.add("entry");
+      entrySection.setAttribute("id", `workout-${i}`);
+      entryContainer.appendChild(entrySection);
+
+      //   // Create the elements to show book data
+      const dateEl = document.createElement("th");
+      const workoutEl = document.createElement("td");
+      const descriptionEl = document.createElement("td");
+      const categoryEl = document.createElement("td");
+
+      //   // Add text to the elements we just created
+      dateEl.textContent = `${date}`;
+      workoutEl.textContent = `${workout}`;
+      descriptionEl.textContent = `${description}`;
+      categoryEl.textContent = `${category}`;
+
+      //   // Append the elements with textContent to the well
+      entrySection.appendChild(dateEl);
+      entrySection.appendChild(workoutEl);
+      entrySection.appendChild(descriptionEl);
+      entrySection.appendChild(categoryEl);
+      entrySection.appendChild(sectionBreak);
+    });
+  })
+  .catch((error) => {
+    console.error("Error:", error);
+  });
