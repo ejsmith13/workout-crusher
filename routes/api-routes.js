2f0f90e9283a1d04328931deb110c3cbcb909aa7
ejsmith13 <ejsmithwork@gmail.com>
2021-03-13 09:20:40 -0500

    posting data to home page

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -53,4 +53,11 @@ module.exports = function(app) {
   app.get("/api/daily", (req, res) => {
     db.Daily.findAll({}).then(results => res.json(results));
   });
+  app.get("/api/daily/:id", (req, res) => {
+    db.Daily.findOne({
+      where: {
+        id: req.params.id
+      }
+    }).then(results => res.json(results));
+  });
 };
